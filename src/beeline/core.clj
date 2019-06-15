0b4a29b7dfd92621ecb81c8972b9ed9d71f6ab19
Adrian Smith <sfyire@gmail.com>
2019-06-15 14:58:55 +0100

    Support function calls WIP

--- src/beeline/core.clj
+++ src/beeline/core.clj
@@ -4,7 +4,7 @@
             [honeysql.core :as sql]
             [clojure.walk :as w]))
 
-(defn fake-keyword
+(defn colon->keyword
   "Converts fake keywords to real keywords"
   [json-key]
   (let [fake? (and (string? json-key)
@@ -14,12 +14,41 @@
         (apply str (rest json-key)))
       json-key)))
 
+(defn nested-callable?
+  "Is this element a nested callable?"
+  [element]
+  (and (vector? element)
+       (= \# (first (first element)))))
+
+(defn call->list
+  "Given the name of a function create a quoted function call"
+  [name & args]
+  (into
+    args
+    (list (resolve (symbol name)))))
+
+(defn function-name
+  "Given a vector fetch the function name"
+  [vector]
+  (apply str (rest (first vector))))
+
+(defn hash->callable
+  "Converts [\"#sql/call\" \":+\" 1 1] to function call"
+  [element]
+  (if (nested-callable? element)
+    (apply
+      call->list
+      (into
+        [(function-name element)]
+        (rest element)))
+    element))
+
 (defn read-json
   "Performing a lossy conversion from json to clj"
   [input]
   (json/read-str
     input
-    :key-fn fake-keyword))
+    :key-fn colon->keyword))
 
 (def example "{\":select\": [\":a\"]}")
 
@@ -29,7 +58,8 @@
   ([input]
    (->> input
        (read-json)
-       (w/postwalk fake-keyword)
+       (w/postwalk colon->keyword)
+       (w/postwalk hash->callable)
        (sql/format)
        (json/write-str)
        (print))))
